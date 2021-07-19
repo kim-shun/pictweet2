f2de749dc7ca7e12d7b0754771629944a0015a71
kim-shun <kimura1417080@gmail.com>
2021-07-19 09:29:27 +0900

    新規登録できない場合のテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,18 +19,42 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
     it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'nicknameが7文字以上では登録できない' do
+      @user.nickname = 'aaaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Nickname is too long (maximum is 6 characters)')
     end
     it '重複したemailが存在する場合登録できない' do
+      @user.save
+      anothor_user = FactoryBot.build(:user)
+      anothor_user.email = @user.email
+      anothor_user.valid?
+      expect(anothor_user.errors.full_messages).to include('Email has already been taken')
     end
     it 'passwordが5文字以下では登録できない' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
   end
 end
