30c81e809a20606b8d9e144c2ef4c79ec41a02ec
kim-shun <kimura1417080@gmail.com>
2021-07-20 13:40:42 +0900

    コメントのテストコードの微修正

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'コメント投稿', type: :system do
     visit new_user_session_path
     fill_in 'Email', with: @tweet.user.email
     fill_in 'Password', with: @tweet.user.password
-    find('input[name="commit"]')
-    expect(current_path).to eq root_path
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
     # ツイート詳細ページに遷移する
     visit tweet_path(@tweet)
     # フォームに情報を入力する
@@ -22,8 +22,8 @@ RSpec.describe 'コメント投稿', type: :system do
       find('input[name="commit"]').click
     }.to change { Comment.count }.by(1)
     # 詳細ページにリダイレクトされることを確認する
-    visit tweet_path(@tweet)
+    expect(current_path).to eq(tweet_path(@tweet))
     # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
-    expect(page).to have_content("#{@comment.text}")
+    expect(page).to have_content @comment
   end
 end
